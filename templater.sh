336acf647d91defa03a6ee2e0e25ab2c01a6c51b
suopea <suopea@pm.me>
2025-04-17 12:17:36 +0300

    isalpha

--- templater.sh
+++ templater.sh
@@ -3,7 +3,7 @@ echo "\n$1\tft_$2($3)\n{}" > ft_$2.c
 
 
 cp test_template test_$2.c
-sed -i -e "s/FUNCTION/ft_$2/g" test_$2.c
+sed -i -e "s/FUNCTION/$2/g" test_$2.c
 sed -i -e "s/input/$3/g" test_$2.c
 sed -i -e "s/correct/$1 correct/g" test_$2.c
 
---
+++ test_isalpha.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <limits.h>
+#include <ctype.h>
+#include "libft.h"
+
+void	test(int c)
+{
+	if (ft_isalpha(c) == (isalpha(c) != 0))
+	{
+		printf("%c success\n", c);
+	}
+	else
+	{
+		printf("%c FAIL :(\n", c);
+	}
+}
+
+int main(void)
+{
+	test('a');
+	test('b');
+	test('A');
+	test('B');
+	test('z');
+	test('z' + 1);
+	test('Z' + 1);
+	test('a' - 1);
+	test('A' - 1);
+	test('1');
+	test('\n');
+	test('\0');
+	test('0');
+	test(-1);
+	test(255);
+	return(0);	
+}
