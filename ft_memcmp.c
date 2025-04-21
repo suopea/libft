8af1a426ed022b2379084f7fec73b57d49e44666
suopea <suopea@pm.me>
2025-04-21 16:07:34 +0300

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssuopea <ssuopea@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 15:29:19 by ssuopea           #+#    #+#             */
+/*   Updated: 2025/04/21 15:46:36 by ssuopea          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t		i;
+	const char	*c1;
+	const char	*c2;
+
+	c1 = s1;
+	c2 = s2;
+	i = 0;
+	while (i < n)
+	{
+		if (c1[i] != c2[i])
+			return (c1[i] - c2[i]);
+		i++;
+	}
+	return (0);
+}
