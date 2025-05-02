8a0feaacdb80279106549975e0d223319b58f38a
suopea <suopea@pm.me>
2025-05-02 15:01:24 +0300

    strlcopy ready

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ssuopea <ssuopea@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 14:23:53 by ssuopea           #+#    #+#             */
-/*   Updated: 2025/05/01 17:06:48 by ssuopea          ###   ########.fr       */
+/*   Updated: 2025/05/02 15:00:05 by ssuopea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	i;
 	size_t	len;
 
-	if (size == 0 || ft_strlen(src))
-		len = 0;
-	else
-		len = ft_strlen(src);
+	len = ft_strlen(src);
 	i = 0;
 	while (i + 1 < size && size > 0 && src[i])
 	{
