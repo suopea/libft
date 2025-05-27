984fe28e44573ac5e545ce56432fef0cae701b1c
suopea <suopea@pm.me>
2025-05-27 13:50:13 +0300

    moved tests elesewhere

--- tests/templater.sh
+++
@@ -1,8 +0,0 @@
-
-echo "$1\tft_$2($3)\n{}" > ft_$2.c
-
-cp ./tests/test_$4.c ./tests/test_$2.c
-sed -i -e "s/$4/$2/g" ./tests/test_$2.c
-sed -i -e "s/void\ttest(.*)/void\ttest($3)/" ./tests/test_$2.c
-
-sed -i -e "0,/;/ s//;\n$1\tft_$2($3);/" libft.h
